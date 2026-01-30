d725f4eec6ee9e7561427305776ffaeb698696ca
Thiw <wiriyaphat6789@gmail.com>
2026-01-30 16:15:00 +0700

    Lab15_5 complete

--- lab15_5.cpp
+++ lab15_5.cpp
@@ -1,22 +1,31 @@
-#include <iostream>
-#include <cstdlib>
-#include <ctime>
-using namespace std;
+	#include <iostream>
+	#include <cstdlib>
+	#include <ctime>
+	using namespace std;
 
-void shuffle(int *,int *,int *,int *);
+	void shuffle(int *,int *,int *,int *);
 
-int main(){
-	int a = 50, b = 100, c = 500, d = 1000;
-	
-	srand(time(0));	
-	
-	for(int i = 0;i < 10;i++){
-	    shuffle(&a,&b,&c,&d);
-	    cout << a << " " << b << " " << c << " " << d << endl;
+	int main(){
+		int a = 50, b = 100, c = 500, d = 1000;
+		
+		srand(time(0));	
+		
+		for(int i = 0;i < 10;i++){
+			shuffle(&a,&b,&c,&d);
+			cout << a << " " << b << " " << c << " " << d << endl;
+		}
+		
+		return 0;
 	}
-	
-	return 0;
-}
 
-//Do not modify the code above this line
-//Write definition of shuffle() using pointer after this line. 
+	//Do not modify the code above this line
+	//Write definition of shuffle() using pointer after this line. 
+	void shuffle(int *a,int *b,int *c,int *d){
+		int *arr[] = {a,b,c,d};
+		for (int i = 3; i>0; i--){
+			int j = rand()%3;
+			int temp = *arr[i];
+			*arr[i] = *arr[j];
+			*arr[j] = temp;
+		}
+	}
