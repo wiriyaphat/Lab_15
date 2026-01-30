e60d5129040a7bfadef62f429887dbf332e6bc47
Thiw <wiriyaphat6789@gmail.com>
2026-01-30 16:14:37 +0700

    Lab15_4 complete

--- lab15_4.cpp
+++ lab15_4.cpp
@@ -19,3 +19,15 @@ int main(){
 }
 
 //Do not modify source code above this line
+void shuffle(int &a,int &b,int &c,int &d){
+	int *arr[] = {&a,&b,&c,&d};
+
+	for (int i = 3;i > 0;i--){
+		int j = rand()%i;
+		int temp = *arr[i];
+		*arr[i] = *arr[j];
+		*arr[j] = temp;
+	}
+	
+
+}
